b1a9f8b3356767092fb880d8c2a61c0d334a8f7f
katojun <jkky0225@gmail.com>
2022-10-09 14:43:35 +0900

    axios追加

---
+++ pages/post.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1>記事一覧</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  async asyncData({ $axios }: any) {
+    const posts = await $axios.$get(
+      "https://jsonplaceholder.typicode.com/posts/"
+    );
+    return { posts };
+  },
+}
+</script>
